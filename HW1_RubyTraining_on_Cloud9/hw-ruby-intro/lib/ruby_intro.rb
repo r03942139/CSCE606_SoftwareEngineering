fb8e5fba25e29fa937313caa18a7359f7b176d67
EC2 Default User <ec2-user@ip-172-31-11-243.us-east-2.compute.internal>
2020-01-29 01:56:05 +0000

    Update_20200128_Rev3

--- HW1_RubyTraining_on_Cloud9/hw-ruby-intro/lib/ruby_intro.rb
+++ HW1_RubyTraining_on_Cloud9/hw-ruby-intro/lib/ruby_intro.rb
@@ -4,29 +4,36 @@
 
 def sum arr
   # YOUR CODE HERE
-  if arr.length == 0 then 0
-  else arr.inject(0) { |sum, elem| sum + elem }
+  if arr.length == 0 
+    then 0
+  else
+    arr.inject(0) { |sum, elem| sum + elem }
   end
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
-  if arr.length == 0 then 0
-  elsif arr.length == 1 then arr[0]
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
   else
     sum = 0
     n = arr.size
     sort_arr = arr.sort
 	  sum = sort_arr[n-1]+sort_arr[n-2]
   end
+  return sum
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
   (0..arr.length-1).each do |i|
-      (i+1..arr.length-1).each do |j|
-          return true if arr[i] + arr[j] == n
+    (i+1..arr.length-1).each do |j|
+      if arr[i] + arr[j] == n
+        return true
       end
+    end
   end
   return false
 end
@@ -42,17 +49,21 @@ def starts_with_consonant? s
   # YOUR CODE HERE
   v = s.downcase
   "bcdfghjklmnpqrstvxz".each_char{ |letter|
-      return true if v[0] == letter }
+    return true if v[0] == letter }
   return false
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
-  return false if s.empty?
+  if s.empty?
+    return false 
+  end
   s.each_char{ |c|
-      return false if c != "1" and c != "0"}
-  return true if s.to_i(2) % 4 == 0
-  false # false in any other case
+    return false if c != "1" and c != "0"}
+  if s.to_i(2) % 4 == 0
+    return true
+  end
+  return false # false in any other case
 end
 
 # Part 3
@@ -62,13 +73,17 @@ class BookInStock
   attr_accessor :isbn, :price
 
   def initialize isbn, price
-      if isbn.empty? then raise ArgumentError end
+      if isbn.empty? 
+        then raise ArgumentError
+      end
       @isbn = isbn
-      if price <= 0 then raise ArgumentError end
+      if price <= 0
+        then raise ArgumentError 
+      end
       @price = price
   end
 
   def price_as_string
-      "$#{format("%.2f", @price)}"
+    "$#{format("%.2f", @price)}"
   end
 end
